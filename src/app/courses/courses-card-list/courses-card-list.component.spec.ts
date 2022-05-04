2b25d2af85c76d28d559567f1517bbe23d1d683e
Fernando Medeiros do Nascimento <fernando_m_n@hotmail.com>
2022-05-04 08:55:17 -0300

    Implementing 'should display the course list' test

--- src/app/courses/courses-card-list/courses-card-list.component.spec.ts
+++ src/app/courses/courses-card-list/courses-card-list.component.spec.ts
@@ -1,10 +1,15 @@
+import { DebugElement } from "@angular/core";
 import { ComponentFixture, TestBed, waitForAsync } from "@angular/core/testing";
+import { By } from "@angular/platform-browser";
+
+import { setupCourses } from "../common/setup-test-data";
 import { CoursesModule } from "./../courses.module";
 import { CoursesCardListComponent } from "./courses-card-list.component";
 
 describe("CoursesCardListComponent", () => {
   let component: CoursesCardListComponent;
   let fixture: ComponentFixture<CoursesCardListComponent>;
+  let el: DebugElement;
 
   beforeEach(waitForAsync(() => {
     TestBed.configureTestingModule({
@@ -14,16 +19,22 @@ describe("CoursesCardListComponent", () => {
       .then(() => {
         fixture = TestBed.createComponent(CoursesCardListComponent);
         component = fixture.componentInstance;
+        el = fixture.debugElement;
       });
   }));
 
   it("should create the component", () => {
     expect(component).toBeTruthy();
-    console.log(component);
   });
 
   it("should display the course list", () => {
-    pending();
+    component.courses = setupCourses();
+    fixture.detectChanges();
+
+    const cards = el.queryAll(By.css(".course-card"));
+
+    expect(cards).toBeTruthy("Could not find cards");
+    expect(cards.length).toBe(12, "Unexpected number of courses");
   });
 
   it("should display the first course", () => {
