38b862c6be3690aa7ecd60a7cd9457b0dcf7fa76
Fernando Medeiros do Nascimento <fernando_m_n@hotmail.com>
2022-05-25 09:36:56 -0300

    Studying async tests

--- src/app/courses/home/async-examples.spec.ts
+++ src/app/courses/home/async-examples.spec.ts
@@ -1,10 +1,47 @@
-describe("Async Testing Examples", () => {
-  it("Asynchronous test example with Jasmine done()", () => {
+import { fakeAsync, flush, tick } from "@angular/core/testing";
+
+fdescribe("Async Testing Examples", () => {
+  it("Asynchronous test example with Jasmine done()", (done: DoneFn) => {
     let test = false;
 
     setTimeout(() => {
+      console.log("running asertions");
       test = true;
       expect(test).toBeTruthy();
+      done();
     }, 1000);
+  });
+
+  it("Asynchronous test example - setTimeout()", fakeAsync(() => {
+    let test = false;
+    setTimeout(() => {});
+
+    setTimeout(() => {
+      console.log("running asertions setTimeout()");
+      test = true;
+    }, 1000);
+
+    flush();
+    expect(test).toBeTruthy();
+  }));
+
+  fit("Asynchronous test example - plain promise", () => {
+    let test = false;
+
+    console.log("creating promise");
+
+
+    Promise.resolve()
+      .then(() => {
+        console.log("Promise first then() evaluated successfully");
+        return Promise.resolve();
+      })
+      .then(() => {
+        console.log("Promise second then() evaluated successfully");
+        test = true;
+      });
+
+    console.log("running test assertions");
+    expect(test).toBeTruthy();
   });
 });
