edd3bd6b2f4f9bb43c0f555672978d7b7a664fdc
Fernando Medeiros do Nascimento <fernando_m_n@hotmail.com>
2022-06-08 16:37:14 -0300

    Implementing Promises+setTimeout and Observable tests

--- src/app/courses/home/async-examples.spec.ts
+++ src/app/courses/home/async-examples.spec.ts
@@ -1,4 +1,6 @@
-import { fakeAsync, flush, flushMicrotasks } from "@angular/core/testing";
+import { fakeAsync, flush, flushMicrotasks, tick } from "@angular/core/testing";
+import { of } from "rxjs";
+import { delay } from "rxjs/operators";
 
 fdescribe("Async Testing Examples", () => {
   it("Asynchronous test example with Jasmine done()", (done: DoneFn) => {
@@ -25,7 +27,7 @@ fdescribe("Async Testing Examples", () => {
     expect(test).toBeTruthy();
   }));
 
-  fit("Asynchronous test example - plain promise", fakeAsync(() => {
+  it("Asynchronous test example - plain promise", fakeAsync(() => {
     let test = false;
 
     console.log("creating promise");
@@ -45,5 +47,40 @@ fdescribe("Async Testing Examples", () => {
 
     console.log("running test assertions");
     expect(test).toBeTruthy();
+  }));
+
+  it("Asynchronous test example - Promises + setTimeout()", fakeAsync(() => {
+    let counter = 0;
+
+    Promise.resolve().then(() => {
+      counter += 10;
+
+      setTimeout(() => {
+        counter += 1;
+      }, 1000);
+    });
+
+    expect(counter).toBe(0);
+
+    flushMicrotasks();
+    expect(counter).toBe(10);
+    tick(500);
+    expect(counter).toBe(10);
+    tick(500);
+    expect(counter).toBe(11);
+  }));
+
+  it("Asynchronous test example - Observables ", fakeAsync(() => {
+    let test = false;
+    console.log("Creating Observable");
+    const test$ = of(test).pipe(delay(1000));
+
+    test$.subscribe(() => {
+      test = true;
+    });
+    tick(1000);
+
+    console.log("Running test assertions");
+    expect(test).toBe(true);
   }));
 });
